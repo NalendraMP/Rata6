2e47e27261a19fb5d848284eba982e5209e58ac7
NalendraMP <145744064+NalendraMP@users.noreply.github.com>
2023-11-02 20:42:36 +0700

    Add files via upload

---
+++ ratarata.py
@@ -0,0 +1,14 @@
+
+def hitung_rata(A, C):
+    ratarata = (A+C)/2
+    return ratarata
+    
+A = float(input("Masukan nilai A :"))
+C = float(input("Masukan nilai C :"))
+
+print("rata rata adalah", hitung_rata(A, C))
+    
+
+
+
+
